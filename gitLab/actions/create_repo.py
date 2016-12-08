7651af4e6d04fed4071367c657bf20c38e2526e8
root <root@ip-172-31-16-23.ap-southeast-1.compute.internal>
2016-12-08 11:38:06 +0000

    Added gitLab pack

---
+++ gitLab/actions/create_repo.py
@@ -0,0 +1,33 @@
+from st2actions.runners.pythonrunner import Action
+import gitlab
+class creategitLabProject(Action):
+      def run(self,repoName):
+        gitLabUrl=self.config["url"]
+	gitLabUserName=self.config["username"]
+        gitLabPassword=self.config["password"]
+        connectGitLab = gitlab.Gitlab(gitLabUrl, email=gitLabUserName, password=gitLabPassword)	
+        self.logger.info('Connecting to GitLab')
+	connectGitLab.auth()
+        self.logger.info(connectGitLab.auth())
+        getAllGitLabProjects=connectGitLab.projects.list()
+        print dir(getAllGitLabProjects[0]) 
+        getAllGitLabProjectNames=[]
+        for gitLabProject in getAllGitLabProjects:
+		getAllGitLabProjectNames.append(gitLabProject.name) 
+        if repoName in getAllGitLabProjectNames:
+		 self.logger.error("Repo exists")
+        else:    
+		gitLabRepoCreationStatus=connectGitLab.projects.create({'name': repoName, 'default_branch': 'master', 'wiki_enabled': 1})
+                print("Iam here")
+                self.logger.info('Created repo')
+                self.logger.info(gitLabRepoCreationStatus)
+		gitLabProjectId=gitLabRepoCreationStatus.id
+                self.logger.info('Creating gitFlow branches')                
+		self.logger.info('Creating develop Branch')
+                connectGitLab.project_files.create({'project_id' :gitLabProjectId,'file_path' :"README.md",'branch_name' :"master",'content':"This is readme",'commit_message':"This initial commit"})
+		gitLabDevelopBranch=connectGitLab.project_branches.create({'branch_name': 'develop','ref': 'master', 'project_id' :gitLabProjectId})
+                branches = connectGitLab.project_branches.list(project_id=gitLabProjectId)
+		self.logger.info(branches)
+
+
+
