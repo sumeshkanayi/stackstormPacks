208f7245e4d603b5380d8622d8feab29e0e1605c
sumeshkanayi <sumeshkanayi@gmail.com>
2017-05-17 09:07:53 +0530

    Update set.sh

--- DevOps/set.sh
+++ DevOps/set.sh
@@ -1,4 +1,4 @@
-authToken=$(st2 auth st2admin -p Ch@ngeMe|grep "token"|awk '{print $4}')
+authToken=$(st2 auth st2admin -p Just4DevOps|grep "token"|awk '{print $4}')
 
 echo "export ST2_AUTH_TOKEN=$authToken"
 
